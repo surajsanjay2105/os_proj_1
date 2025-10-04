6d25ca2e361073cd713dd5c88ed1c3409ea1e3e4
Suraj Sanjy <suraj@Surajs-MacBook-Pro.local>
2025-10-03 21:10:05 -0700

    changes

--- proj1/kernel/calculate.c
+++ proj1/kernel/calculate.c
@@ -10,18 +10,15 @@ sys_calculate(void)
   uint64 result_ptr;
   int result;
 
-  // Get arguments from user space
   if(argint(0, &x) < 0 || argint(1, &y) < 0 || argaddr(3, &result_ptr) < 0)
     return -1;
 
-  // Get operator from user space
   uint64 op_ptr;
   if(argaddr(2, &op_ptr) < 0)
     return -1;
   if(copyin(myproc()->pagetable, &op, op_ptr, 1) < 0)
     return -1;
 
-  // Validate operator and perform calculation
   switch(op) {
     case '+':
       result = x + y;
@@ -34,14 +31,13 @@ sys_calculate(void)
       break;
     case '/':
       if(y == 0)
-        return -1;  // Division by zero error
+        return -1;
       result = x / y;
       break;
     default:
-      return -1;  // Invalid operator
+      return -1;
   }
 
-  // Copy result back to user space
   if(copyout(myproc()->pagetable, result_ptr, (char*)&result, sizeof(result)) < 0)
     return -1;
 
